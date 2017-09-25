0ba31aa14daeedf023f4937440229e10883b7e7d
sdbabys <stephenlkeeley@gmail.com>
2017-09-25 15:53:08 -0400

    created repo for fourier python functions

---
+++ comp_fourier.py
@@ -0,0 +1,155 @@
+import numpy as np
+import warnings
+
+from . import mkcovs, kron_ops
+from . import fft_ops as rffb
+
+
+def conv_fourier(x,dims,minlens,nxcirc = None,condthresh = 1e8):
+	# Compute least-squares regression sufficient statistics in DFT basis
+	# Version of this NOT complete for higher dimensions 9/15/17!
+	#
+	# INPUT:
+	# -----
+	#           x [D x n x p] - stimulus, where each row vector is the spatial stim at a single time, D is number of batch
+	#        dims [m x 1] - number of coefficients along each stimulus dimension
+	#     minlens [m x 1] - minimum length scale for each dimension (can be scalar)
+	#      nxcirc [m x 1] - circular boundary in each stimulus dimension (minimum is dims) OPTIONAL
+	#  condthresh [1 x 1] - condition number for thresholding for small eigenvalues OPTIONAL
+	#
+	# OUTPUT:
+	# ------
+	#     Bx  - output data, x, in fourier domain
+	#  wwnrm [nf x 1] - squared "effective frequencies" in vector form for each dim
+	#   Bfft  {1 x p} - cell array with DFT bases for each dimension
+	# 	1e8 is default value (condition number on prior covariance)
+
+
+	dims = np.array(np.reshape(dims,(1,-1)))
+	minlens = np.array(np.reshape(minlens,(1,-1)))
+
+	# Set circular bounardy (for n-point fft) to avoid edge effects, if needed
+	if nxcirc is None:
+	    #nxcirc = np.ceil(max([dims(:)'+minlens(:)'*4; dims(:)'*1.25]))'
+	    nxcirc = np.ceil(np.max(np.concatenate((dims+minlens*4 ,dims*1), axis = 0), axis = 0))
+
+
+	nd = np.size(dims) # number of filter dimensions
+	if np.size(minlens) is 1 and nd is not 1: #% make vector out of minlens, if necessary
+	    minlens = np.repmat(minlens,nd,1)
+
+
+	# Loop through dimensions
+
+	#None of these quantities depend on the data directly
+	wvecs = [rffb.comp_wvec(nxcirc[jj],minlens[0][jj], condthresh) for jj in np.arange(nd)]
+	#cdiagvecs = [mkcovs.mkcovdiag_ASD(minlens[jj],1,nxcirc[jj],np.square(wvecs[jj]))  for jj in np.arange(nd)]
+	Bffts = [rffb.realfftbasis(dims[jj],nxcirc[jj],wvecs[jj])[0] for jj in np.arange(nd)]
+
+
+	#Note... These previous lines could have been done with one function...mkcovs.mkcov_ASDfactored, but due to ease of eye
+	# and a desire to use list comprehensions, each component is split into it's own function. This also allows for an
+	#easier tracking of each individual part, should you need to follow the steps....
+
+
+	#fprintf('\n Total # Fourier coeffs represented: %d\n\n', prod(ncoeff));
+
+	def f(switcher):  
+	    # switch based on stimulus dimension
+	    if switcher is 2:
+	    	pass
+	    if switcher is 3:
+	    	pass
+	    return{
+	    1: #% 1 dimensional stimulus
+	         [np.square(2*np.pi/nxcirc[0]) * np.square(wvecs[0]), #normalized wvec
+	         np.ones([np.size(wvecs[0]),1])==1] #indices to keep 
+
+	        
+	    # 2: % 2 dimensional stimulus
+	        
+	    #     % Form full frequency vector and see which to cut
+	    #     Cdiag = kron(cdiagvecs{2},cdiagvecs{1});
+	    #     ii = (Cdiag/max(Cdiag))>1/condthresh; % indices to keep 
+	                    
+	    #     % compute vector of normalized frequencies squared
+	    #     [ww1,ww2] = ndgrid(wvecs{1},wvecs{2});
+	    #     wwnrm = [(ww1(ii)*(2*pi/nxcirc(1))).^2 ...
+	    #         (ww2(ii)*(2*pi/nxcirc(2))).^2];
+	        
+	    # 3: % 3 dimensional stimulus
+
+	    #     Cdiag = kron(cdiagvecs{3},(kron(cdiagvecs{2},cdiagvecs{1})));
+	    #     ii = (Cdiag/max(Cdiag))>1/condthresh; % indices to keep
+	        
+	    #     % compute vector of normalized frequencies squared
+	    #     [ww1,ww2,ww3] = ndgrid(wvecs{1},wvecs{2},wvecs{3});
+	    #     wwnrm = [(ww1(ii)*(2*pi/nxcirc(1))).mv ^2, ...
+	    #         (ww2(ii)*(2*pi/nxcirc(2))).^2, ....,
+	    #         (ww3(ii)*(2*pi/nxcirc(3))).^2];
+	        
+	    # otherwise
+	    #     error('compLSsuffstats_fourier.m : doesn''t yet handle %d dimensional filters\n',nd);
+		}[switcher] 
+	try:
+	    [wwnrm, ii] = f(nd)
+	except KeyError:
+	    print('\n\n Does not handle values of dimension', nd, 'yet')    
+	
+
+	# Calculate stimulus sufficient stats in Fourier domain
+
+	
+	# if x.shape[0] == 1: 
+
+	# 	#originally this used the transpose operation (kronmulttrp) ! !!!might be a transpositional issue.
+	# 	Bx = kron_ops.kronmult(Bffts,np.transpose(x)) # convert to Fourier domain
+	# 	Bx = Bx[ii] # prune unneeded freqs
+
+	# elif x.shape[0]>1: #Batched data. when the shape of x is 3 and dims is 2, for example. 
+
+	Bx = [kron_ops.kronmult(Bffts,np.transpose(batch)) for batch in x]
+	Bx = [prune[ii] for prune in Bx]
+
+
+
+	return Bx, wwnrm, Bffts, ii, nxcirc, wvecs
+
+
+
+
+def conv_fourier_batch(x,dims,minlens,nxcirc = None,condthresh = 1e8):
+
+	if len(x.shape) <= len(dims):
+		warnings.warn('\n\n shape of input vector is not longer than dims vector. Try using conv_fourier, not conv_fourier_batch \n\n')
+
+	#return a list of arrays for the Bx all the batched data, 	
+	return [conv_fourier(batch,dims,minlens,nxcirc,condthresh)[0] 
+	for batch in arange(x)] + [conv_fourier(x[0],dims,minlens,nxcirc,condthresh)[1,:]]
+
+
+
+
+def compLSsuffstats_fourier(x,y,dims,minlens,nxcirc = None,condthresh = 1e8):
+	# Compute least-squares regression sufficient statistics in DFT basis
+
+	[Bx, wwnrm, Bfft, ii] = conv_fourier(x,dims,minlens,nxcirc, condthresh)
+
+	dd = {}
+	dd['xx'] = np.matmul(Bx,Bx, transpose_b=True)
+	dd['xy'] = Bx * y
+
+	# Fill in other statistics
+	dd['yy'] = np.matmul(np.transpose(y),y) # marginal response variance
+	dd['nsamps'] = np.size(y,1)  # total number of samples
+
+	return dd, wwnrm, Bfft, ii 
+
+
+
+
+
+
+
+
+
---
+++ mkcovs.py
@@ -0,0 +1,160 @@
+import numpy as np
+import warnings
+
+from . import fft_ops as rffb
+import tensorflow as tf
+
+def mkcovdiag_ASD(len_sc,rho,nxcirc,wvec= None,wwnrm= None, calc_derivs = None):
+#  Eigenvalues of ASD covariance (as diagonalized in Fourier domain)
+# 
+#  [cdiag,dcdiag,ddcdiag] = mkcovdiag_ASD(rho,l,nxcirc,wvecsq)
+# 
+#  Compute discrete ASD (RBF kernel) eigenspectrum using frequencies in [0, nxcirc].
+#  See mkCov_ASD_factored for more info
+# 
+#  INPUT (all python 1d lists!):
+#          len - length scale of ASD kernel (determines smoothness)
+#          rho - maximal prior variance ("overall scale")
+#       nxcirc - number of coefficients to consider for circular boundary 
+#         wvecsq - vector of squared frequencies for DFT 
+#         
+#  OUTPUT:
+#      cdiag [nxcirc x 1] - vector of eigenvalues of C for frequencies in w
+#     dcinv [nxcirc x 2] - 1st derivs [dC^-1/drho, dC^-1/dlen]
+#        dc [nxcirc x 2] - 1st derivs [dC / drho , dC / dlen]
+#     ddcinv [nxcirc x 3] - 2nd derivs of C-1 w.r.t [drho^2, drho*dlen, dlen^2]
+# 
+# Note: nxcirc = nx corresponds to having a circular boundary 
+
+
+# Compute diagonal of ASD covariance matrix
+	if wvec is not None:
+		wvecsq = np.square(wvec)
+		const = np.square(2*np.pi/nxcirc) # constant 
+		ww = wvecsq*const  # effective frequency vector
+	elif wwnrm is not None:
+		ww = wwnrm
+	else:
+		print("please provide either wvec or a normalized wvec into this function")
+
+
+
+
+
+	cdiag = np.sqrt(2*np.pi)*rho*len_sc*np.exp(-.5*ww*np.square(len_sc))
+
+	# 1st derivative of inv(Cdiag)
+	if calc_derivs is not None:
+	####NOT WORKING YET!!!	(shouldn't really need this if using tensorflow...)
+
+	    nw = np.size(wvecsq)
+	    dcinv = [(-1/len_sc + len_sc*ww)/cdiag,-(1/rho)/cdiag] # dC^-1/drho
+
+
+	# 1st derivative of Cdiag 
+	    dc = [(1/len_sc - len_sc*ww)*cdiag[:,1, 1], (1./rho)*np.ones(nw,1)] # dC/drho
+	         # dC/dl
+
+
+	# 2nd derivative of inv(Cdiag)
+	    ddcinv = ([(2/np.square(len_sc) - ww + np.square(len_sc)*np.square(ww)), # d^2 C^-1 /dl^2
+	        (1/rho)*(1/len_sc - len_sc*ww),   # d^2 C^-1 /drho dl
+	        (2/np.square(rho))*np.ones(nw,1)]     # d^2 C^-1 /drho^2
+	        / cdiag[:,np.ones(1,3)])
+
+	        
+
+
+	    return cdiag, dcinv, dc, ddcinv
+
+	elif calc_derivs is None:
+		return cdiag
+
+
+
+
+def mkcov_ASDfactored(prs,nx,nxcirc=None,condthresh = 1e8,compfftbasis = None):
+# % Factored representation of ASD covariance matrix in Fourier domain
+# %
+# % [Cdiag,U,wvec] = mkcov_ASDfactored(prs,nx,opts)
+# %
+# % Covariance represented as C = U*sdiag*U'
+# % where U is unitary (in some larger basis) and sdiag is diagonal
+# %
+# %  C_ij = rho*exp(((i-j)^2/(2*l^2))
+# %
+# % INPUT:
+# % ------
+# %   prs [2 x 1]  - ASD parameters [len_sc = length scale; rho - maximal variance; ]:
+# %    nx [1 x 1]  - number of regression coeffs
+# % 
+# % Note: nxcirc = nx gives circular boundary
+# %
+# % OUTPUT:
+# % -------
+# %   cdiag [ni x 1] - vector with thresholded eigenvalues of C
+# %       U [ni x nxcirc] - column vectors define orthogonal basis for C (on Reals)
+# %    wvec [nxcirc x 1] - vector of Fourier frequencies
+# %      ii [nxcirc x 1] - binary vector indicating which frequencies included in U, sdiag
+
+	len_sc = prs[0]
+	rho = prs[1]
+
+	# % Parse inputs
+	if nxcirc is None:
+		nxcirc = nx + np.ceil(4*len_sc) # extends support by 4 stdevs of ASD kernel width
+
+
+
+	# % Check that nxcirc isn't bigger than nx
+	if nxcirc < nx:
+	    warnings.warn('mkcov_ASDfactored: nxcirc < nx. Some columns of x will be ignored')
+
+
+	# % compute vector of Fourier frequencies
+	# maxfreq = np.floor(nxcirc/(np.pi*len_sc)*np.sqrt(.5*np.log(condthresh))) # max
+	# if maxfreq < nxcirc/2:
+	#     wvec = np.concatenate(([np.arange(int(maxfreq))],[np.arange(-int(maxfreq),0)]),axis = 1)
+	#else:
+	    # % in case cutoff is above max number of frequencies
+	wvec = rffb.comp_wvec(nxcirc, len_sc, condthresh)
+
+
+
+	# % Compute diagonal in Fourier domain
+	cdiag = mkcovdiag_ASD(len_sc,rho,nxcirc,wvec = wvec) # compute diagonal and Fourier freqs
+
+	# % Compute real-valued discrete Fourier basis U
+	if compfftbasis is not None:
+	    U = rffb.realfftbasis(nx,nxcirc,wvec)[0]
+	    return cdiag, U, wvec
+	else:
+		return cdiag
+
+
+
+def mkcovdiag_ASD_tf(len_sc,rho,nxcirc,wvec= None,wwnrm= None):
+#  Eigenvalues of ASD covariance (as diagonalized in Fourier domain)
+# 
+#  Identical to mkcovdiag_ASD except using tensorflow operations.
+#  No need for computing of derivatives here
+
+
+# Compute diagonal of ASD covariance matrix
+	if wvec is not None:
+		wvecsq = np.square(wvec)
+		const = np.square(2*np.pi/nxcirc) # constant 
+		ww = wvecsq*const  # effective frequency vector
+	elif wwnrm is not None:
+		ww = wwnrm
+	else:
+		print("please provide either wvec or a normalized wvec into this function")
+
+	cdiag = tf.sqrt(2*np.pi)*rho*len_sc*tf.exp(-.5*ww*tf.square(len_sc))
+
+	
+	return cdiag
+
+
+
+	
