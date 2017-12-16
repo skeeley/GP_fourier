bad8f3a87c63917e4061edaa3ce1566655247637
Stephen Keeley <StephenLKeeley@gmail.com>
2017-12-15 22:06:13 -0500

    cleaned a little for sharing

--- comp_fourier.py
+++ comp_fourier.py
@@ -6,12 +6,11 @@ from . import fft_ops as rffb
 
 
 def conv_fourier(x,dims,minlens,nxcirc = None,condthresh = 1e8):
-	# Compute least-squares regression sufficient statistics in DFT basis
 	# Version of this NOT complete for higher dimensions 9/15/17!
 	#
 	# INPUT:
 	# -----
-	#           x [D x n x p] - stimulus, where each row vector is the spatial stim at a single time, D is number of batch
+	#           x [D x n x p] - stimulus, where each row vector is the spatial stim at a single time, D is number of batches 
 	#        dims [m x 1] - number of coefficients along each stimulus dimension
 	#     minlens [m x 1] - minimum length scale for each dimension (can be scalar)
 	#      nxcirc [m x 1] - circular boundary in each stimulus dimension (minimum is dims) OPTIONAL
@@ -20,8 +19,8 @@ def conv_fourier(x,dims,minlens,nxcirc = None,condthresh = 1e8):
 	# OUTPUT:
 	# ------
 	#     Bx  - output data, x, in fourier domain
-	#  wwnrm [nf x 1] - squared "effective frequencies" in vector form for each dim
-	#   Bfft  {1 x p} - cell array with DFT bases for each dimension
+	#  wwnrm [nf x 1] - squared "effective frequencies" in vector form for each dim (normalized)
+	#   Bfft  {1 x p} - cell array with DFT bases for each dimension (list of numpy arrays for each dimension)
 	# 	1e8 is default value (condition number on prior covariance)
 
 
@@ -38,18 +37,13 @@ def conv_fourier(x,dims,minlens,nxcirc = None,condthresh = 1e8):
 	if np.size(minlens) is 1 and nd is not 1: #% make vector out of minlens, if necessary
 	    minlens = np.repmat(minlens,nd,1)
 
-
-	# Loop through dimensions
-
+	# generate here a list of your
 	#None of these quantities depend on the data directly
 	wvecs = [rffb.comp_wvec(nxcirc[jj],minlens[0][jj], condthresh) for jj in np.arange(nd)]
 	#cdiagvecs = [mkcovs.mkcovdiag_ASD(minlens[jj],1,nxcirc[jj],np.square(wvecs[jj]))  for jj in np.arange(nd)]
 	Bffts = [rffb.realfftbasis(dims[jj],nxcirc[jj],wvecs[jj])[0] for jj in np.arange(nd)]
 
 
-	#Note... These previous lines could have been done with one function...mkcovs.mkcov_ASDfactored, but due to ease of eye
-	# and a desire to use list comprehensions, each component is split into it's own function. This also allows for an
-	#easier tracking of each individual part, should you need to follow the steps....
 
 
 	#fprintf('\n Total # Fourier coeffs represented: %d\n\n', prod(ncoeff));
@@ -111,7 +105,7 @@ def conv_fourier(x,dims,minlens,nxcirc = None,condthresh = 1e8):
 	Bx = [kron_ops.kronmult(Bffts,np.transpose(batch)) for batch in x]
 	Bx = [prune[ii] for prune in Bx]
 
-	return Bx, wwnrm, Bffts, ii, nxcirc, wvecs
+	return Bx, wwnrm, Bffts, nxcirc
 
 
 
--- mkcovs.py
+++ mkcovs.py
@@ -4,7 +4,7 @@ import warnings
 from . import fft_ops as rffb
 import tensorflow as tf
 
-def mkcovdiag_ASD(len_sc,rho,nxcirc,wvec= None,wwnrm= None, calc_derivs = None):
+def mkcovdiag_ASD(len_sc,rho,nxcirc,wvec= None,wwnrm= None):
 #  Eigenvalues of ASD covariance (as diagonalized in Fourier domain)
 # 
 #  [cdiag,dcdiag,ddcdiag] = mkcovdiag_ASD(rho,l,nxcirc,wvecsq)
@@ -16,13 +16,11 @@ def mkcovdiag_ASD(len_sc,rho,nxcirc,wvec= None,wwnrm= None, calc_derivs = None):
 #          len - length scale of ASD kernel (determines smoothness)
 #          rho - maximal prior variance ("overall scale")
 #       nxcirc - number of coefficients to consider for circular boundary 
-#         wvecsq - vector of squared frequencies for DFT 
+#         wvec - vector of freq for DFT 
+#		wwnrm - vector of freq for DFT (normalized)
 #         
 #  OUTPUT:
 #      cdiag [nxcirc x 1] - vector of eigenvalues of C for frequencies in w
-#     dcinv [nxcirc x 2] - 1st derivs [dC^-1/drho, dC^-1/dlen]
-#        dc [nxcirc x 2] - 1st derivs [dC / drho , dC / dlen]
-#     ddcinv [nxcirc x 3] - 2nd derivs of C-1 w.r.t [drho^2, drho*dlen, dlen^2]
 # 
 # Note: nxcirc = nx corresponds to having a circular boundary 
 
@@ -37,37 +35,8 @@ def mkcovdiag_ASD(len_sc,rho,nxcirc,wvec= None,wwnrm= None, calc_derivs = None):
 	else:
 		print("please provide either wvec or a normalized wvec into this function")
 
-
-
-
 	cdiag = np.sqrt(2*np.pi)*rho*len_sc*np.exp(-.5*ww*np.square(len_sc))
-
-	# 1st derivative of inv(Cdiag)
-	if calc_derivs is not None:
-	####NOT WORKING YET!!!	(shouldn't really need this if using tensorflow...)
-
-	    nw = np.size(wvecsq)
-	    dcinv = [(-1/len_sc + len_sc*ww)/cdiag,-(1/rho)/cdiag] # dC^-1/drho
-
-
-	# 1st derivative of Cdiag 
-	    dc = [(1/len_sc - len_sc*ww)*cdiag[:,1, 1], (1./rho)*np.ones(nw,1)] # dC/drho
-	         # dC/dl
-
-
-	# 2nd derivative of inv(Cdiag)
-	    ddcinv = ([(2/np.square(len_sc) - ww + np.square(len_sc)*np.square(ww)), # d^2 C^-1 /dl^2
-	        (1/rho)*(1/len_sc - len_sc*ww),   # d^2 C^-1 /drho dl
-	        (2/np.square(rho))*np.ones(nw,1)]     # d^2 C^-1 /drho^2
-	        / cdiag[:,np.ones(1,3)])
-
-	        
-
-
-	    return cdiag, dcinv, dc, ddcinv
-
-	elif calc_derivs is None:
-		return cdiag
+	return cdiag
 
 
 
@@ -94,7 +63,6 @@ def mkcov_ASDfactored(prs,nx,nxcirc=None,condthresh = 1e8,compfftbasis = None):
 # %   cdiag [ni x 1] - vector with thresholded eigenvalues of C
 # %       U [ni x nxcirc] - column vectors define orthogonal basis for C (on Reals)
 # %    wvec [nxcirc x 1] - vector of Fourier frequencies
-# %      ii [nxcirc x 1] - binary vector indicating which frequencies included in U, sdiag
 
 	len_sc = prs[0]
 	rho = prs[1]
@@ -165,7 +133,6 @@ def mkcovdiag_ASD_wellcond_tf(len_sc,rho,nxcirc,addition = 1e-8, wvec= None,wwnr
 #  Eigenvalues of ASD covariance (as diagonalized in Fourier domain)
 # 
 #  Identical to mkcovdiag_ASD except using tensorflow operations.
-#  No need for computing of derivatives here
 
 
 # Compute diagonal of ASD covariance matrix
@@ -189,7 +156,6 @@ def mkcovdiag_ASD_nD_tf(len_sc,rho,nxcirc, condthresh, wvec= None,wwnrm= None):
 #  Eigenvalues of ASD covariance (as diagonalized in Fourier domain)
 # 
 #  Identical to mkcovdiag_ASD except using tensorflow operations.
-#  No need for computing of derivatives here
 # Compute diagonal of ASD covariance matrix.
 # threshold again based on length scale!!!
 	if wvec is not None:
